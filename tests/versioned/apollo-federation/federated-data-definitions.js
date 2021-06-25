6d4fa6f7c7909a383dc86777adc456c09d4907f7
Michael Goin <mgoin@newrelic.com>
2021-06-24 17:01:03 -0700

    Fixes segment nesting for Apollo Federation Gateway operations.

---
+++ tests/versioned/apollo-federation/federated-data-definitions.js
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2021 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+/* --- Example Query ---
+query MyQuery {
+  libraries {
+    branch
+    booksInStock {
+      isbn,
+      title,
+      author
+    }
+    magazinesInStock {
+      issue,
+      title
+    }
+  }
+}
+*/
+
+const libraries = [
+  {
+    id: '1',
+    branch: 'downtown'
+  },
+  {
+    id: '2',
+    branch: 'riverside'
+  },
+  {
+    id: '3',
+    branch: 'northwest crossing'
+  },
+  {
+    id: '4',
+    branch: 'east'
+  }
+]
+
+function _getLibraryTypeDef(gql) {
+  const typeDefs = gql`
+    type Library @key(fields: "id") {
+      id: ID!
+      branch: String
+    }
+
+    extend type Query {
+      library(id: ID!): Library
+      libraries: [Library]
+    }
+    `
+
+  return typeDefs
+}
+
+// https://www.apollographql.com/docs/federation/api/apollo-federation/#__resolvereference
+function _getLibraryResolvers() {
+  const resolvers = {
+    Library: {
+      __resolveReference(reference) {
+        return libraries.find((library) => library.id === reference.id)
+      }
+    },
+    Query: {
+      library(_, { id }) {
+        return libraries.find((library) => library.id === id)
+      },
+      libraries() {
+        return libraries
+      }
+    }
+  }
+
+  return resolvers
+}
+
+const books = [
+  {
+    title: 'Node Agent: The Book',
+    isbn: 'a-fake-isbn',
+    author: 'Sentient Bits',
+    branch: '2'
+  },
+  {
+    title: 'Ollies for O11y: A Sk8er\'s Guide to Observability',
+    isbn: 'a-second-fake-isbn',
+    author: 'Faux Hawk',
+    branch: '1'
+  },
+  {
+    title: '[Redacted]',
+    isbn: 'a-third-fake-isbn',
+    author: 'Closed Telemetry',
+    branch: '2'
+  },
+  {
+    title: 'Be a hero: fixing the things you broke',
+    isbn: 'a-fourth-fake-isbn',
+    author: '10x Developer',
+    branch: '1'
+  }
+]
+
+function _getBookTypeDef(gql) {
+  const typeDefs = gql`
+    type Book {
+      isbn: ID!
+      title: String
+      author: String
+      branch: Library
+    }
+
+    extend type Library @key(fields: "id") {
+      id: ID! @external
+      booksInStock: [Book]
+    }
+
+    extend type Query {
+      book(isbn: ID!): Book
+      books: [Book]
+    }
+  `
+  return typeDefs
+}
+
+function _getBookResolvers() {
+  const resolvers = {
+    Book: {
+      branch(parent) {
+        return { __typename: 'Library', id: parent.branch }
+      }
+    },
+    Library: {
+      booksInStock(parent) {
+        return books.filter((book) => book.branch === parent.id)
+      }
+    },
+    Query: {
+      book(_, { isbn }) {
+        return books.find((book) => book.isbn === isbn)
+      },
+      books() {
+        return books
+      }
+    }
+  }
+
+  return resolvers
+}
+
+const magazines = [
+  {
+    title: 'Reli Updates Weekly',
+    issue: 1,
+    branch: '2'
+  },
+  {
+    title: 'Reli Updates Weekly',
+    issue: 2,
+    branch: '1'
+  }
+]
+
+function _getMagazineTypeDef(gql) {
+  const typeDefs = gql`
+    type Magazine {
+      issue: ID!
+      title: String
+      branch: Library
+    }
+
+    extend type Library @key(fields: "id") {
+      id: ID! @external
+      magazinesInStock: [Magazine]
+    }
+
+    extend type Query {
+      magazine(issue: ID!): Magazine
+      magazines: [Magazine]
+    }
+  `
+
+  return typeDefs
+}
+
+function _getMagazineResolvers() {
+  const resolvers = {
+    Magazine: {
+      branch(parent) {
+        return { __typename: 'Library', id: parent.branch }
+      }
+    },
+    Library: {
+      magazinesInStock(parent) {
+        return magazines.filter((magazine) => magazine.branch === parent.id)
+      }
+    },
+    Query: {
+      magazine(_, { issue }) {
+        return magazines.find((magazine) => magazine.issue === issue)
+      },
+      magazines() {
+        return magazines
+      }
+    }
+  }
+
+  return resolvers
+}
+
+function getLibraryConfiguration(gql) {
+  const config = {
+    name: 'Library',
+    typeDefs: _getLibraryTypeDef(gql),
+    resolvers: _getLibraryResolvers()
+  }
+
+  return config
+}
+
+function getBookConfiguration(gql) {
+  const config = {
+    name: 'Book',
+    typeDefs: _getBookTypeDef(gql),
+    resolvers: _getBookResolvers()
+  }
+
+  return config
+}
+
+function getMagazineConfiguration(gql) {
+  const config = {
+    name: 'Magazine',
+    typeDefs: _getMagazineTypeDef(gql),
+    resolvers: _getMagazineResolvers()
+  }
+
+  return config
+}
+
+module.exports = {
+  getLibraryConfiguration,
+  getBookConfiguration,
+  getMagazineConfiguration
+}
---
+++ tests/versioned/apollo-federation/segments.test.js
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2020 New Relic Corporation. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict'
+
+const { executeQuery, executeQueryBatch } = require('../../test-client')
+
+const ANON_PLACEHOLDER = '<anonymous>'
+const OPERATION_PREFIX = 'GraphQL/operation/ApolloServer'
+const EXTERNAL_PREFIX = 'External'
+
+const { setupFederatedGatewayServerTests } = require('./federated-gateway-server-setup')
+
+setupFederatedGatewayServerTests({
+  suiteName: 'fastify segments',
+  createTests: createFederatedSegmentsTests
+})
+
+/**
+ * Creates a set of standard segment naming and nesting tests to run
+ * against express-based apollo-server libraries.
+ * It is required that t.context.helper and t.context.serverUrl are set.
+ * @param {*} t a tap test instance
+ */
+function createFederatedSegmentsTests(t, frameworkName) {
+  const TRANSACTION_PREFIX = `WebTransaction/${frameworkName}/POST`
+
+  t.test('should nest sub graphs under operation', (t) => {
+    const { helper, serverUrl } = t.context
+
+    const query = `query {
+      libraries {
+        branch
+        booksInStock {
+          isbn,
+          title,
+          author
+        }
+        magazinesInStock {
+          issue,
+          title
+        }
+      }
+    }`
+
+    helper.agent.on('transactionFinished', (transaction) => {
+      if (transaction.forceIgnore) {
+        return
+      }
+
+      const libraryExternal = formatExternalSegment(t.context.libraryUrl)
+      const bookExternal = formatExternalSegment(t.context.bookUrl)
+      const magazineExternal = formatExternalSegment(t.context.magazineUrl)
+
+      const operationPart = `query/${ANON_PLACEHOLDER}`
+      const expectedSegments = [{
+        name: `${TRANSACTION_PREFIX}//${operationPart}`,
+        children: [{
+          name: 'Expressjs/Router: /',
+          children: [{
+            name: 'Nodejs/Middleware/Expressjs/<anonymous>',
+            children: [{
+              name: `${OPERATION_PREFIX}/${operationPart}`,
+              children: [
+                { name: libraryExternal },
+                { name: bookExternal },
+                { name: magazineExternal }
+              ]
+            }]
+          }]
+        }]
+      }]
+
+      t.segments(transaction.trace.root, expectedSegments)
+    })
+
+    executeQuery(serverUrl, query, (err, result) => {
+      t.error(err)
+      checkResult(t, result, () => {
+        t.end()
+      })
+    })
+  })
+
+  t.test('batch query should nest sub graphs under appropriate operations', (t) => {
+    const { helper, serverUrl } = t.context
+
+    const booksQueryName = 'GetBooksForLibraries'
+    const booksQuery = `query ${booksQueryName} {
+      libraries {
+        branch
+        booksInStock {
+          isbn,
+          title,
+          author
+        }
+      }
+    }`
+
+    const magazineQueryName = 'GetMagazinesForLibraries'
+    const magazineQuery = `query ${magazineQueryName} {
+      libraries {
+        branch
+        magazinesInStock {
+          issue,
+          title
+        }
+      }
+    }`
+
+    const queries = [booksQuery, magazineQuery]
+
+    const libraryExternal = formatExternalSegment(t.context.libraryUrl)
+    const bookExternal = formatExternalSegment(t.context.bookUrl)
+    const magazineExternal = formatExternalSegment(t.context.magazineUrl)
+
+    helper.agent.on('transactionFinished', (transaction) => {
+      if (transaction.forceIgnore) {
+        return
+      }
+
+      const operationPart1 = `query/${booksQueryName}`
+      const expectedQuery1Name = `${operationPart1}`
+      const operationPart2 = `query/${magazineQueryName}`
+      const expectedQuery2Name = `${operationPart2}`
+
+      const batchTransactionPrefix = `${TRANSACTION_PREFIX}//batch`
+
+      const expectedSegments = [{
+        name: `${batchTransactionPrefix}/${expectedQuery1Name}/${expectedQuery2Name}`,
+        children: [{
+          name: 'Expressjs/Router: /',
+          children: [{
+            name: 'Nodejs/Middleware/Expressjs/<anonymous>',
+            children: [
+              {
+                name: `${OPERATION_PREFIX}/${operationPart1}`,
+                children: [
+                  { name: libraryExternal },
+                  { name: bookExternal }
+                ]
+              },
+              {
+                name: `${OPERATION_PREFIX}/${operationPart2}`,
+                children: [
+                  { name: libraryExternal },
+                  { name: magazineExternal }
+                ]
+              }
+            ]
+          }]
+        }]
+      }]
+
+      t.segments(transaction.trace.root, expectedSegments)
+    })
+
+    executeQueryBatch(serverUrl, queries, (err, result) => {
+      t.error(err)
+      checkResult(t, result, () => {
+        t.equal(result.length, 2)
+
+        t.end()
+      })
+    })
+  })
+}
+
+/**
+ * Verify we didn't break anything outright and
+ * test is setup correctly for functioning calls.
+ */
+ function checkResult(t, result, callback) {
+  t.ok(result)
+
+  if (result.errors) {
+    result.errors.forEach((error) => {
+      t.error(error)
+    })
+  }
+
+  setImmediate(callback)
+}
+
+function formatExternalSegment(url) {
+  const hostAndPort = url.replace('http://', '')
+  const name = `${EXTERNAL_PREFIX}/${hostAndPort}`
+
+  return name
+}
