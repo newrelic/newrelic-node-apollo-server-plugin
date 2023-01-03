ba3e5a5eb2aaaa6001950beb655f2bfe60ba2d8b
Jessica Lopatta <jlopatta@newrelic.com>
2023-01-03 10:18:41 -0500

    NEWRELIC-5907 enhanced typing tests

--- tests/types/index.test-d.ts
+++ tests/types/index.test-d.ts
@@ -1,9 +1,17 @@
 import { ApolloServerPlugin as V3Plugin } from "apollo-server-plugin-base";
 import { ApolloServerPlugin as V4Plugin } from "@apollo/server";
-import { expectType } from "tsd";
+import { expectType, expectNotType } from "tsd";
 
 import createPlugin from "../..";
 
+
+interface foo {}
+const plugin: foo = createPlugin({})
+expectNotType<V3Plugin>(plugin)
+expectNotType<V3Plugin>(createPlugin<foo>({}))
+
+const pluginV3: V3Plugin = createPlugin({})
+expectType<V3Plugin>(pluginV3)
 expectType<V3Plugin>(createPlugin<V3Plugin>({}));
 expectType<V3Plugin>(
   createPlugin<V3Plugin>({
@@ -13,6 +21,8 @@ expectType<V3Plugin>(
   })
 );
 
+const pluginV4: V4Plugin = createPlugin({})
+expectType<V4Plugin>(pluginV4)
 expectType<V4Plugin>(createPlugin<V4Plugin>({}))
 expectType<V4Plugin>(
   createPlugin<V4Plugin>({
