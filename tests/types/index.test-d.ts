a84ba2bf01effca40eb98171f7e212ac440b1e66
Zach Tindall <a88zach@gmail.com>
2023-07-26 15:35:25 -0400

    feat: enhance typings for customResolverAttributes and customOperationAttributes

--- tests/types/index.test-d.ts
+++ tests/types/index.test-d.ts
@@ -4,36 +4,45 @@ import { expectType, expectNotType } from "tsd";
 
 import createPlugin from "../..";
 
-
 interface foo {}
-const plugin: foo = createPlugin({})
-expectNotType<V3Plugin>(plugin)
-expectNotType<V3Plugin>(createPlugin<foo>({}))
+const plugin: foo = createPlugin({});
+expectNotType<V3Plugin>(plugin);
+expectNotType<V3Plugin>(createPlugin<foo>({}));
 
-const pluginV3: V3Plugin = createPlugin({})
-expectType<V3Plugin>(pluginV3)
+const pluginV3: V3Plugin = createPlugin({});
+expectType<V3Plugin>(pluginV3);
 expectType<V3Plugin>(createPlugin<V3Plugin>({}));
 expectType<V3Plugin>(
-  createPlugin<V3Plugin>({
+  createPlugin<
+    V3Plugin,
+    { source: any; args: any; context: { bar: string }; info: any },
+    { bar: string }
+  >({
     captureHealthCheckQueries: true,
     captureScalars: false,
     captureServiceDefinitionQueries: true,
     captureFieldMetrics: true,
-    customOperationAttributes: (ctx: any) => ({ key: ctx.foo }),
-    customResolverAttributes: (ctx: any) => ({ attr: ctx.bar })
+    customOperationAttributes: (ctx) => ({
+      attr: ctx.bar,
+    }),
+    customResolverAttributes: (ctx) => ({ attr: ctx.context.bar }),
   })
 );
 
-const pluginV4: V4Plugin = createPlugin({})
-expectType<V4Plugin>(pluginV4)
-expectType<V4Plugin>(createPlugin<V4Plugin>({}))
+const pluginV4: V4Plugin = createPlugin({});
+expectType<V4Plugin>(pluginV4);
+expectType<V4Plugin>(createPlugin<V4Plugin>({}));
 expectType<V4Plugin>(
-  createPlugin<V4Plugin>({
+  createPlugin<
+    V4Plugin,
+    { source: any; args: any; contextValue: { bar: string }; info: any },
+    { bar: string }
+  >({
     captureHealthCheckQueries: true,
     captureScalars: false,
     captureServiceDefinitionQueries: true,
     captureFieldMetrics: true,
-    customOperationAttributes: (ctx: any) => ({ key: ctx.foo }),
-    customResolverAttributes: (ctx: any) => ({ attr: ctx.bar })
+    customOperationAttributes: (ctx) => ({ key: ctx.bar }),
+    customResolverAttributes: (ctx) => ({ attr: ctx.contextValue.bar }),
   })
 );
