81caf929b1addcffb4664d2a01a889b3ef56e269
James Sumners <jsumners@newrelic.com>
2024-12-03 15:02:42 -0500

    update server errors tests

--- tests/versioned/errors-tests.js
+++ tests/versioned/errors-tests.js
@@ -1,28 +1,28 @@
 /*
- * Copyright 2020 New Relic Corporation. All rights reserved.
+ * Copyright 2024 New Relic Corporation. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 'use strict'
 
+const assert = require('node:assert')
+
 const { executeQuery, makeRequest } = require('../test-client')
 const agentTesting = require('../agent-testing')
+const promiseResolvers = require('./promise-resolvers')
 
 const ANON_PLACEHOLDER = '<anonymous>'
 const UNKNOWN_OPERATION = '<unknown>'
-
 const OPERATION_PREFIX = 'GraphQL/operation/ApolloServer'
 const RESOLVE_PREFIX = 'GraphQL/resolve/ApolloServer'
 
-/**
- * Creates a set of standard error capture tests to run against various
- * apollo-server libraries.
- * It is required that t.context.helper and t.context.serverUrl are set.
- * @param {*} t a tap test instance
- */
-function createErrorTests(t, _, isApollo4) {
-  t.test('parsing error should be noticed and assigned to operation span', (t) => {
-    const { helper, serverUrl } = t.context
+const tests = []
+
+tests.push({
+  name: 'parsing error should be noticed and assigned to operation span',
+  async fn(t) {
+    const { helper, serverUrl } = t.nr
+    const { promise, resolve } = promiseResolvers()
 
     const expectedErrorMessage = 'Syntax Error: Expected Name, found <EOF>.'
     const expectedErrorType = 'GraphQLError'
@@ -40,43 +40,49 @@ function createErrorTests(t, _, isApollo4) {
 
     helper.agent.once('transactionFinished', (transaction) => {
       const errorTraces = agentTesting.getErrorTraces(helper.agent)
-      t.equal(errorTraces.length, 1)
+      assert.equal(errorTraces.length, 1)
 
       const errorTrace = errorTraces[0]
 
       const [, transactionName, errorMessage, errorType, params] = errorTrace
-      t.equal(transactionName, transaction.name)
-      t.equal(errorMessage, expectedErrorMessage)
-      t.equal(errorType, expectedErrorType)
+      assert.equal(transactionName, transaction.name)
+      assert.equal(errorMessage, expectedErrorMessage)
+      assert.equal(errorType, expectedErrorType)
 
       const { agentAttributes } = params
 
-      t.ok(agentAttributes.spanId)
+      assert.ok(agentAttributes.spanId)
 
       const matchingSpan = agentTesting.findSpanById(helper.agent, agentAttributes.spanId)
 
       const { attributes, intrinsics } = matchingSpan
-      t.equal(intrinsics.name, `${OPERATION_PREFIX}/${UNKNOWN_OPERATION}`)
-      t.equal(attributes['error.message'], expectedErrorMessage)
-      t.equal(attributes['error.class'], expectedErrorType)
+      assert.equal(intrinsics.name, `${OPERATION_PREFIX}/${UNKNOWN_OPERATION}`)
+      assert.equal(attributes['error.message'], expectedErrorMessage)
+      assert.equal(attributes['error.class'], expectedErrorType)
     })
 
     executeQuery(serverUrl, invalidQuery, (err, result) => {
-      t.error(err)
+      assert.ifError(err)
 
-      t.ok(result)
-      t.ok(result.errors)
-      t.equal(result.errors.length, 1) // should have one parsing error
+      assert.ok(result)
+      assert.ok(result.errors)
+      assert.equal(result.errors.length, 1) // should have one parsing error
 
       const [parseError] = result.errors
-      t.equal(parseError.extensions.code, 'GRAPHQL_PARSE_FAILED')
+      assert.equal(parseError.extensions.code, 'GRAPHQL_PARSE_FAILED')
 
-      t.end()
+      resolve()
     })
-  })
 
-  t.test('validation error should be noticed and assigned to operation span', (t) => {
-    const { helper, serverUrl } = t.context
+    await promise
+  }
+})
+
+tests.push({
+  name: 'validation error should be noticed and assigned to operation span',
+  async fn(t) {
+    const { helper, serverUrl } = t.nr
+    const { promise, resolve } = promiseResolvers()
 
     const expectedErrorMessage = 'Cannot query field "doesnotexist" on type "Book".'
     const expectedErrorType = 'GraphQLError'
@@ -96,43 +102,49 @@ function createErrorTests(t, _, isApollo4) {
 
     helper.agent.once('transactionFinished', (transaction) => {
       const errorTraces = agentTesting.getErrorTraces(helper.agent)
-      t.equal(errorTraces.length, 1)
+      assert.equal(errorTraces.length, 1)
 
       const errorTrace = errorTraces[0]
 
       const [, transactionName, errorMessage, errorType, params] = errorTrace
-      t.equal(transactionName, transaction.name)
-      t.equal(errorMessage, expectedErrorMessage)
-      t.equal(errorType, expectedErrorType)
+      assert.equal(transactionName, transaction.name)
+      assert.equal(errorMessage, expectedErrorMessage)
+      assert.equal(errorType, expectedErrorType)
 
       const { agentAttributes } = params
 
-      t.ok(agentAttributes.spanId)
+      assert.ok(agentAttributes.spanId)
 
       const matchingSpan = agentTesting.findSpanById(helper.agent, agentAttributes.spanId)
 
       const { attributes, intrinsics } = matchingSpan
-      t.equal(intrinsics.name, expectedOperationName)
-      t.equal(attributes['error.message'], expectedErrorMessage)
-      t.equal(attributes['error.class'], expectedErrorType)
+      assert.equal(intrinsics.name, expectedOperationName)
+      assert.equal(attributes['error.message'], expectedErrorMessage)
+      assert.equal(attributes['error.class'], expectedErrorType)
     })
 
     executeQuery(serverUrl, invalidQuery, (err, result) => {
-      t.error(err)
+      assert.ifError(err)
 
-      t.ok(result)
-      t.ok(result.errors)
-      t.equal(result.errors.length, 1) // should have one parsing error
+      assert.ok(result)
+      assert.ok(result.errors)
+      assert.equal(result.errors.length, 1) // should have one parsing error
 
       const [validationError] = result.errors
-      t.equal(validationError.extensions.code, 'GRAPHQL_VALIDATION_FAILED')
+      assert.equal(validationError.extensions.code, 'GRAPHQL_VALIDATION_FAILED')
 
-      t.end()
+      resolve()
     })
-  })
 
-  t.test('resolver error should be noticed and assigned to resolve span', (t) => {
-    const { helper, serverUrl } = t.context
+    await promise
+  }
+})
+
+tests.push({
+  name: 'resolver error should be noticed and assigned to resolve span',
+  async fn(t) {
+    const { helper, serverUrl } = t.nr
+    const { promise, resolve } = promiseResolvers()
 
     const expectedErrorMessage = 'Boom goes the dynamite!'
     const expectedErrorType = 'Error'
@@ -146,62 +158,68 @@ function createErrorTests(t, _, isApollo4) {
 
     helper.agent.once('transactionFinished', (transaction) => {
       const errorTraces = agentTesting.getErrorTraces(helper.agent)
-      t.equal(errorTraces.length, 1)
+      assert.equal(errorTraces.length, 1)
 
       const errorTrace = errorTraces[0]
 
       const [, transactionName, errorMessage, errorType, params] = errorTrace
-      t.equal(transactionName, transaction.name)
-      t.equal(errorMessage, expectedErrorMessage)
-      t.equal(errorType, expectedErrorType)
+      assert.equal(transactionName, transaction.name)
+      assert.equal(errorMessage, expectedErrorMessage)
+      assert.equal(errorType, expectedErrorType)
 
       const { agentAttributes } = params
 
-      t.ok(agentAttributes.spanId)
+      assert.ok(agentAttributes.spanId)
 
       const matchingSpan = agentTesting.findSpanById(helper.agent, agentAttributes.spanId)
 
       const { attributes, intrinsics } = matchingSpan
-      t.equal(intrinsics.name, expectedResolveName)
-      t.equal(attributes['error.message'], expectedErrorMessage)
-      t.equal(attributes['error.class'], expectedErrorType)
+      assert.equal(intrinsics.name, expectedResolveName)
+      assert.equal(attributes['error.message'], expectedErrorMessage)
+      assert.equal(attributes['error.class'], expectedErrorType)
     })
 
     executeQuery(serverUrl, invalidQuery, (err, result) => {
-      t.error(err)
+      assert.ifError(err)
 
-      t.ok(result)
-      t.ok(result.errors)
-      t.equal(result.errors.length, 1) // should have one parsing error
+      assert.ok(result)
+      assert.ok(result.errors)
+      assert.equal(result.errors.length, 1) // should have one parsing error
 
       const [resolverError] = result.errors
-      t.equal(resolverError.extensions.code, 'INTERNAL_SERVER_ERROR')
+      assert.equal(resolverError.extensions.code, 'INTERNAL_SERVER_ERROR')
 
-      t.end()
+      resolve()
     })
-  })
 
-  const errorTests = [
-    {
-      type: 'UserInputError',
-      code: 'BAD_USER_INPUT',
-      name: 'userInputError',
-      msg: 'user input error'
-    },
-    {
-      type: 'ValidationError',
-      code: 'GRAPHQL_VALIDATION_FAILED',
-      name: 'validationError',
-      msg: 'validation error'
-    },
-    { type: 'ForbiddenError', code: 'FORBIDDEN', name: 'forbiddenError', msg: 'forbidden error' },
-    { type: 'SyntaxError', code: 'GRAPHQL_PARSE_FAILED', name: 'syntaxError', msg: 'syntax error' },
-    { type: 'AuthenticationError', code: 'UNAUTHENTICATED', name: 'authError', msg: 'auth error' }
-  ]
+    await promise
+  }
+})
 
-  errorTests.forEach(({ type, code, name, msg }) => {
-    t.test(type, (t) => {
-      const { helper, serverUrl } = t.context
+const errorTests = [
+  {
+    type: 'UserInputError',
+    code: 'BAD_USER_INPUT',
+    name: 'userInputError',
+    msg: 'user input error'
+  },
+  {
+    type: 'ValidationError',
+    code: 'GRAPHQL_VALIDATION_FAILED',
+    name: 'validationError',
+    msg: 'validation error'
+  },
+  { type: 'ForbiddenError', code: 'FORBIDDEN', name: 'forbiddenError', msg: 'forbidden error' },
+  { type: 'SyntaxError', code: 'GRAPHQL_PARSE_FAILED', name: 'syntaxError', msg: 'syntax error' },
+  { type: 'AuthenticationError', code: 'UNAUTHENTICATED', name: 'authError', msg: 'auth error' }
+]
+for (const errorTest of errorTests) {
+  const { type, code, name, msg } = errorTest
+  tests.push({
+    name: type,
+    async fn(t) {
+      const { helper, serverUrl } = t.nr
+      const { promise, resolve } = promiseResolvers()
 
       const expectedErrorMessage = msg
       const expectedErrorType = type
@@ -212,84 +230,96 @@ function createErrorTests(t, _, isApollo4) {
 
       helper.agent.once('transactionFinished', (transaction) => {
         const errorTraces = agentTesting.getErrorTraces(helper.agent)
-        t.equal(errorTraces.length, 1)
+        assert.equal(errorTraces.length, 1)
 
         const errorTrace = errorTraces[0]
 
         const [, transactionName, errorMessage, errorType, params] = errorTrace
-        t.equal(transactionName, transaction.name)
-        t.equal(errorMessage, expectedErrorMessage)
-        t.equal(errorType, expectedErrorType)
+        assert.equal(transactionName, transaction.name)
+        assert.equal(errorMessage, expectedErrorMessage)
+        assert.equal(errorType, expectedErrorType)
 
         const { agentAttributes, userAttributes } = params
 
-        t.ok(agentAttributes.spanId)
-        t.equal(userAttributes.code, code)
+        assert.ok(agentAttributes.spanId)
+        assert.equal(userAttributes.code, code)
 
         const matchingSpan = agentTesting.findSpanById(helper.agent, agentAttributes.spanId)
 
         const { attributes } = matchingSpan
-        t.equal(attributes['error.message'], expectedErrorMessage)
-        t.equal(attributes['error.class'], expectedErrorType)
+        assert.equal(attributes['error.message'], expectedErrorMessage)
+        assert.equal(attributes['error.class'], expectedErrorType)
       })
 
       executeQuery(serverUrl, invalidQuery, (err, result) => {
-        t.error(err)
-        t.ok(result)
-        t.ok(result.errors)
-        t.equal(result.errors.length, 1) // should have one parsing error
+        assert.ifError(err)
+        assert.ok(result)
+        assert.ok(result.errors)
+        assert.equal(result.errors.length, 1) // should have one parsing error
 
         const [resolverError] = result.errors
-        t.equal(resolverError.extensions.code, code)
-        t.end()
+        assert.equal(resolverError.extensions.code, code)
+        resolve()
       })
-    })
-  })
 
-  t.test('Invalid operation name should not crash server', (t) => {
-    const { helper, serverUrl } = t.context
+      await promise
+    }
+  })
+}
+
+tests.push({
+  name: 'Invalid operation name should not crash server',
+  async fn(t) {
+    const {
+      helper,
+      serverUrl,
+      apolloServerPkg: { isApollo4 }
+    } = t.nr
+    const { promise, resolve } = promiseResolvers()
     const query = 'query Hello { hello }'
     const expectedErrorMessage = 'Unknown operation named "testMe".'
     const expectedErrorType = 'GraphQLError'
 
     helper.agent.once('transactionFinished', (transaction) => {
       const errorTraces = agentTesting.getErrorTraces(helper.agent)
-      t.equal(errorTraces.length, 1)
+      assert.equal(errorTraces.length, 1)
 
       const errorTrace = errorTraces[0]
 
       const [, transactionName, errorMessage, errorType, params] = errorTrace
-      t.equal(transactionName, transaction.name)
-      t.equal(errorMessage, expectedErrorMessage)
-      t.equal(errorType, expectedErrorType)
+      assert.equal(transactionName, transaction.name)
+      assert.equal(errorMessage, expectedErrorMessage)
+      assert.equal(errorType, expectedErrorType)
 
       const { agentAttributes } = params
 
-      t.ok(agentAttributes.spanId)
+      assert.ok(agentAttributes.spanId)
 
       const matchingSpan = agentTesting.findSpanById(helper.agent, agentAttributes.spanId)
 
       const { attributes } = matchingSpan
-      t.equal(attributes['error.message'], expectedErrorMessage)
-      t.equal(attributes['error.class'], expectedErrorType)
+      assert.equal(attributes['error.message'], expectedErrorMessage)
+      assert.equal(attributes['error.class'], expectedErrorType)
     })
     const data = JSON.stringify({ query, operationName: 'testMe' })
     makeRequest(serverUrl, data, (err, result) => {
-      t.error(err)
-      t.ok(result)
-      t.ok(result.errors)
-      t.equal(result.errors.length, 1) // should have one parsing error
+      assert.ifError(err)
+      assert.ok(result)
+      assert.ok(result.errors)
+      assert.equal(result.errors.length, 1) // should have one parsing error
       const [resolverError] = result.errors
-      // in apollo 4 they added a first class code for invalid oepration names
+      // in apollo 4 they added a first class code for invalid operation names
       const expectedCode = isApollo4 ? 'OPERATION_RESOLUTION_FAILURE' : 'INTERNAL_SERVER_ERROR'
-      t.equal(resolverError.extensions.code, expectedCode)
-      t.equal(resolverError.message, expectedErrorMessage)
-      t.end()
+      assert.equal(resolverError.extensions.code, expectedCode)
+      assert.equal(resolverError.message, expectedErrorMessage)
+      resolve()
     })
-  })
-}
+
+    await promise
+  }
+})
 
 module.exports = {
-  suiteName: 'errors',
-  createTests: createErrorTests
+  tests,
+  suiteName: 'errors'
 }
