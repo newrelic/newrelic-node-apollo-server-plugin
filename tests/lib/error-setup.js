0dedbd8f56d22064d4861c9ee269e0b3e3baf058
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2025-07-29 16:59:18 -0500

    restore error classes

--- tests/lib/error-setup.js
+++ tests/lib/error-setup.js
@@ -5,6 +5,67 @@
 
 'use strict'
 
+function createErrorClasses(serverPkgExport) {
+  const { GraphQLError } = serverPkgExport.graphql
+
+  class CustomError extends GraphQLError {
+    constructor(message) {
+      super(message)
+      this.extensions.code = 'CUSTOM_ERROR'
+      this.name = 'CustomError'
+    }
+  }
+
+  class ForbiddenError extends GraphQLError {
+    constructor(message) {
+      super(message)
+      this.extensions.code = 'FORBIDDEN'
+      this.name = 'ForbiddenError'
+    }
+  }
+
+  class SyntaxError extends GraphQLError {
+    constructor(message) {
+      super(message)
+      this.extensions.code = 'GRAPHQL_PARSE_FAILED'
+      this.name = 'SyntaxError'
+    }
+  }
+
+  class UserInputError extends GraphQLError {
+    constructor(message) {
+      super(message)
+      this.extensions.code = 'BAD_USER_INPUT'
+      this.name = 'UserInputError'
+    }
+  }
+
+  class ValidationError extends GraphQLError {
+    constructor(message) {
+      super(message, { extensions: { code: 'GRAPHQL_VALIDATION_FAILED' } })
+      this.extensions.code = 'GRAPHQL_VALIDATION_FAILED'
+      this.name = 'ValidationError'
+    }
+  }
+
+  class AuthenticationError extends GraphQLError {
+    constructor(message) {
+      super(message)
+      this.extensions.code = 'UNAUTHENTICATED'
+      this.name = 'AuthenticationError'
+    }
+  }
+
+  return {
+    CustomError,
+    ForbiddenError,
+    SyntaxError,
+    UserInputError,
+    ValidationError,
+    AuthenticationError
+  }
+}
+
 /**
  * Defines a few resolves that throw different types of errors
  *
@@ -20,7 +81,7 @@ module.exports = function setupErrorResolvers(serverPkgExport, resolvers) {
     UserInputError,
     ValidationError,
     AuthenticationError
-  } = serverPkgExport
+  } = createErrorClasses(serverPkgExport)
 
   resolvers.Query.boom = () => {
     throw new Error('Boom goes the dynamite!')
